34b80441c5b0d47dbc4293114700c75b9527cf07
silviosnjr <silviosnjr@gmail.com>
2024-06-20 16:38:01 -0300

    código da reuniao 5

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ const perguntas = [
         ]
     } 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
